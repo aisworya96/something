031db1391ff51caf0818c717663449332262941d
Girijasankar <apple@Girijas-Mac.local>
2023-04-26 19:36:06 +0530

    First commit

---
+++ sum.py
@@ -0,0 +1,23 @@
+def sum(a, b):
+    return 5
+
+
+def sum2():
+    a = 2
+    b = 3
+    print(a + b)
+
+def sum3():
+    a = 20
+    b = 30
+    print(a + b)
+
+def sum4():
+    a = 20
+    b = 300
+    print(a + b)
+
+def sum4():
+    a = 200
+    b = 300
+    print(a + b)
